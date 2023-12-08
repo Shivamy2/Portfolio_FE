a585c4b9c06e688529be674478effec8e24a0cda
shivam.yadav <shivam.yadav@geminisolutions.com>
2023-12-08 15:29:45 +0530

    removed unused console

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,6 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 const queryClient = new QueryClient();
-console.log(queryClient);
 root.render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
